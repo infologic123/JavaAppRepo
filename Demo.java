89ba1b02914e0258bd36decb3a05481c33f1e10e
infologic123 <infologicaws@gmail.com>
2024-11-23 15:17:05 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo{
+   public static void main(String[] args){
+      int x=5;
+      int y=6;
+      System.out.println(x*y);
+      System.out.println("Hello Satya welcome to world...");
+   }
+}
+
+
+
